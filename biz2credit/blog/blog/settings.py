befc824f97a1b398b3618f25cfa58b239f9526aa
Tarun <tarun@indiqus.com>
2019-08-25 19:11:36 +0530

    JWT addition

--- biz2credit/blog/blog/settings.py
+++ biz2credit/blog/blog/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'widget_tweaks',
-    'django_tables2',
-    'django_filters',
+#    'widget_tweaks',
+#    'django_tables2',
+#    'django_filters',
 ]
 
 
